dfb5ea53603ed73cf171a35cd678041e8dd58f90
gaurav.pawar <pawargaurav1256@gmail.com>
2025-08-16 16:17:27 +0530

    footer remaining

--- src/App.js
+++ src/App.js
@@ -4,43 +4,84 @@ import './App.css';
 function App() {
   return (
     <div>
-    <table border="1">
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Image</th>
-      </tr>
-      <tr>
-        <td className='blackbackground'>Gaurav</td>
-        <td className='whitebackground'>Pawar</td>
-        <td><img src="../assets/rohit Sharma odi.webp" width={200} ></img></td>
-      </tr>
-      <tr>
-        <td className='whitebackground'>Shardul</td>
-        <td className='blackbackground'>Pawar</td>
-        <td><img src="../assets/rohit Sharma odi.webp" width={200}></img></td>
-      </tr>
-      <tr>
-        <td className='blackbackground'>Shravani</td>
-        <td className='whitebackground'>Gunjal</td>
-        <td><img src="../assets/rohit Sharma odi.webp" width={200}></img></td>
-      </tr>
-      <tr>
-        <td className='whitebackground'>Trusha</td>
-        <td className='blackbackground'>Nimse</td>
-        <td><img src="../assets/RohitSharma4.webp" width={200}></img></td>
-      </tr>
-      <tr>
-        <td className='blackbackground'>Rutuja</td>
-        <td className='whitebackground'>Date</td>
-        <td><img src="../assets/rohit Sharma odi.webp" width={200}></img></td>
-      </tr>
-      <tr>
-        <td className='whitebackground'>Anamika</td>
-        <td className='blackbackground'>Kute</td>
-        <td><img src="../assets/rohit Sharma odi.webp" width={200}></img></td>
-      </tr>
-    </table>
+      <div className='headersection'>
+        <img src="https://3extent.vercel.app/Assets/3extent.png" width="80px" />
+        <div className='headerlinks'>
+          <div className='links'>Patner With Us</div>
+          <div className='links'>About Us</div>
+          <div className='links'>Contact Us</div>
+        </div>
+      </div>
+      <div className='secondsection'>
+        <div className='backgroundimgtext'>
+          Certified Second-Hand Mobiles<br />
+          Fully tested, graded, and ready for resale
+        </div>
+      </div>
+      <div className='thirdsection'>
+        At 3_EXTENT Pvt. Ltd., we specialize in the bulk supply of high-quality, pre-owned smartphones to resellers, retailers, and businesses across India. With a strong focus on quality, affordability, and trust, we help you grow your business by giving you access to the best devices at competitive wholesale prices.
+      </div>
+      <div className='fourthsection'>
+        <div className='fourthsectioninfo'>We specialize in B2B wholesale trading of high-quality second-hand and refurbished mobile phones. Since our inception, we've been committed to delivering reliable, affordable, and premium-grade devices to retailers, resellers, and bulk buyers across India.</div>
+        <div>
+          <img src="	https://3extent.vercel.app/Assets/home2.webp" className='fourthsectionimgs' />
+        </div>
+      </div>
+      <div className='fourthsection'>
+        <div>
+          <img src="	https://3extent.vercel.app/Assets/home3.webp" className='fourthsectionimgs' />
+        </div>
+        <div className='fourthsectioninfo'>
+          With a deep understanding of the mobile market and strong supplier networks, we ensure that every device we sell is thoroughly tested, quality-checked, and competitively priced. Our goal is to help our clients maximize their margins while giving customers access to the latest smartphones at the best value.
+        </div>
+      </div>
+      <div className='fourthsection'>
+        <div className='fourthsectioninfo'>In just a short span, we've built a reputation for trust, transparency, and timely delivery, selling thousands of units and serving a growing network of satisfied partners. Whether you are a local shop owner or a large-scale distributor, we provide the products and service you can depend on.</div>
+        <div>
+          <img src="https://3extent.vercel.app/Assets/home4.webp" className='fourthsectionimgs' />
+        </div>
+      </div>
+      <div className='fifthsection'>
+        <div className='partnerwithus'>Partner With Us</div>
+        <div className='sentence'>Looking to stock up on reliable smartphones for resale? Partner with 3_EXTENT Pvt. Ltd. and get access to a consistent, profitable, and growing supply chain.</div>
+      </div>
+      <div className='partnerwithus'>
+        About Us
+      </div>
+      <div className='allpage'>
+        <div className='aboutUspage'>
+          <img src='https://3extent.vercel.app/Assets/CEO.jpeg' className='ceoimg' />
+          <div className='postname'>FOUNDER, Chief Executive Officer (CEO)</div>
+          <div className='name'>Hrushikesh Tangadkar</div>
+        </div>
+        <div className='aboutUspage'>
+          <img src='https://3extent.vercel.app/Assets/CTO.jpg' className='ceoimg' />
+          <div className='postname'>FOUNDER, Chief Technology Officer (CTO)</div>
+          <div className='name'>Dipak Dhawade</div>
+        </div>
+        <div className='aboutUspage'>
+          <img src='https://3extent.vercel.app/Assets/CGO.jpeg' className='ceoimg' />
+          <div className='postname'>FOUNDER, Chief Growth Officer (CGO)</div>
+          <div className='name'>Vinod Gade</div>
+        </div>
+      </div>
+
+      <div classname="sixthsection">
+        <h2>Our Mission</h2>
+        <p>
+          To become India’s most trusted wholesale partner for second-hand and refurbished mobiles
+          by consistently<br/> delivering quality, value, and customer satisfaction.
+        </p>
+
+        <h2>Our Values</h2>
+        <div class="values">
+          <p><strong>Quality First –</strong> Every device is tested for performance and reliability.</p>
+          <p><strong>Integrity in Business –</strong> We believe in honest pricing and transparent deals.</p>
+          <p><strong>Partnership Focused –</strong> Your success is our success.</p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
